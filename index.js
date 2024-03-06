b92c8a788b4446cd2e44a6105bd618e20deda993
lenkaanuradha <lenkaanuradha149@gmail.com>
2024-03-06 23:48:33 +0530

    lastchange

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
-
+app.use(cors());
 const port = 5000
 const mongoDB = require('./db');
 mongoDB();
-app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","https://gofoodbackend-2-tw5g.onrender.com")
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Resquested-With, Content-Type, Accept"
-  );
-  next();
-})
+ 
 app.use(express.json());
 app.use('/api',require('./Routes/DisplayData'))
 app.use('/api',require('./Routes/CreatUser'))
